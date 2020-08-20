d9d174d6605372b871415096a0214071a87ffa34
ScottORLY <scott.orlyck+orly@gmail.com>
2020-08-20 16:07:05 -0700

    add appstore link

--- src/index.js
+++ src/index.js
@@ -12,12 +12,14 @@ document.body.appendChild(
         </div>
         <div id={styles.images}>
             <img className={styles.image} src={phone} />
-            <a><img className={styles.image} src={badge} /></a>
+            <a href='https://apps.apple.com/us/app/monte-carlo-xo/id1527959414'>
+                <img className={styles.image} src={badge} />
+            </a>
         </div>
         <div id={styles.privacy} >
             <h1>Privacy Policy</h1>
             <p>
-                Tic Tac Toe does not require any personally identifiable information whatsoever. 
+                Tic-tac-toe does not require any personally identifiable information whatsoever. 
                 Monte Carlo XO does collect, store or transmit any personal information.
             </p>
         </div>
